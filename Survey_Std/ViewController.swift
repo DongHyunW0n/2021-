bb6d6b3c5b421faa74ea16391e4bc99a3d9ccb6c
DongHyunW0n <nexon320@gmail.com>
2021-06-01 06:59:19 -0700

    Initial Commit

---
+++ Survey_Std/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Survey_Std
+//
+//  Created by 원동현 on 2021/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
