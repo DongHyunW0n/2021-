55bd8c21c3a16ee94946e9fbc97f1be364761458
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2021-06-06 01:44:24 +0900

    스크롤뷰 구현완료

--- Survey_Std/ViewController.swift
+++ Survey_Std/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     
   
+    @IBOutlet weak var button_login: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
+   
+    @IBAction func login_button(_ sender: Any) {//로그인 버튼을 누르면 생기는 일들
+        let vcName = self.storyboard?.instantiateViewController(identifier: "start_Controller")
+        vcName?.modalPresentationStyle = .fullScreen//전체화면으로 보이게
+        
+        vcName?.modalTransitionStyle = .crossDissolve//애니메이션 삽입
+        self.present(vcName!, animated: true, completion: nil)
+        
+    }
     
 }
 
